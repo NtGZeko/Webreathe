4e3c6fb108d5576b6c51c68921070ee23e57a3f0
NtGZeko <matheooz@live.fr>
2022-11-24 13:14:46 +0100

    create header

--- index.php
+++ index.php
@@ -8,7 +8,25 @@
   <link href="style.css" rel="stylesheet">
   <title>Webreathe</title>
 </head>
-
+<header>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">visualisation</a>
+          </li>
+      </div>
+    </div>
+  </nav>
+</header>
 <body>
 
 </body>
